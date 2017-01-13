f9ded2e6fac8d694d0888e219105da9c74d6c55f
forax <forax@univ-mlv.fr>
2017-01-13 10:08:23 +0100

    remove unnecessary import

--- src/main/java/com.github.forax.pro.ubermain/com/github/forax/pro/ubermain/Main.java
+++ src/main/java/com.github.forax.pro.ubermain/com/github/forax/pro/ubermain/Main.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.io.UncheckedIOException;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodHandles.Lookup;
