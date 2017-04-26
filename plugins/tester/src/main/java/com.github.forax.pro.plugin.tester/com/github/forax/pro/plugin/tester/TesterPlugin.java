68d3dfcd35f5d9c7610b985fc7232fc720a8831a
Christian Stein <sormuras@gmail.com>
2017-04-26 10:45:32 +0200

    Scan well-known exploded test folders for tests and move FileHelperTest

--- plugins/tester/src/main/java/com.github.forax.pro.plugin.tester/com/github/forax/pro/plugin/tester/TesterPlugin.java
+++ plugins/tester/src/main/java/com.github.forax.pro.plugin.tester/com/github/forax/pro/plugin/tester/TesterPlugin.java
@@ -8,6 +8,14 @@ import com.github.forax.pro.api.Config;
 import com.github.forax.pro.api.MutableConfig;
 import com.github.forax.pro.api.Plugin;
 import com.github.forax.pro.api.WatcherRegistry;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
 import org.junit.platform.console.ConsoleLauncher;
 import org.junit.platform.engine.TestEngine;
 
@@ -34,19 +42,48 @@ public class TesterPlugin implements Plugin {
     ByteArrayOutputStream out = new ByteArrayOutputStream();
     ByteArrayOutputStream err = new ByteArrayOutputStream();
 
+    String[] options = buildDefaultConsoleLauncherOptions();
+    Arrays.stream(options).forEach(System.out::println);
+
     Thread.currentThread().setContextClassLoader(TestEngine.class.getClassLoader());
     int exitCode = ConsoleLauncher.execute(
         new PrintStream(out, true, "UTF-8"),
         new PrintStream(err, true, "UTF-8"),
-        "--include-engine", "junit-jupiter",
-        "--scan-classpath",
-        "--classpath", "target/test/exploded/com.github.forax.pro.helper",
-        "--classpath", "plugins/tester/target/test/exploded/com.github.forax.pro.plugin.tester"
+        options
     ).getExitCode();
 
     System.out.println(out);
     System.err.println(err);
 
     return exitCode;
+  }
+
+  String[] buildDefaultConsoleLauncherOptions() {
+    List<String> options = new ArrayList<>();
+    // scan available exploded test directories
+    options.add("--scan-classpath");
+    Consumer<String> addClassPath = path -> {
+      options.add("--classpath");
+      options.add(path);
+    };
+    findDirectoriesAt("target/test/exploded").forEach(addClassPath);
+    findDirectoriesAt("plugins")
+        .forEach(plugin -> findDirectoriesAt(plugin + "/target/test/exploded")
+            .forEach(addClassPath));
+    return options.toArray(new String[options.size()]);
+  }
+
+  List<String> findDirectoriesAt(String base) {
+    List<String> directories = new ArrayList<>();
+    Path parent = Paths.get(base);
+    DirectoryStream.Filter<Path> filter = path -> path.toFile().isDirectory();
+    try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(parent, filter)) {
+      for (Path path : directoryStream) {
+        directories.add(path.toString());
+      }
+    } catch (IOException ex) {
+      // ignore
+    }
+    return directories;
   }
 }
