9b2ad11529b92efa5324561f2c4c0edfa5ac109c
Christian Stein <sormuras@gmail.com>
2017-05-07 02:14:31 +0200

    Fix compiler warnings and activate plugins test run

--- plugins/tester/src/main/java/com.github.forax.pro.plugin.tester/module-info.java
+++ plugins/tester/src/main/java/com.github.forax.pro.plugin.tester/module-info.java
@@ -2,17 +2,7 @@ module com.github.forax.pro.plugin.tester {
   requires com.github.forax.pro.api;
   requires com.github.forax.pro.helper;
 
-  // java.lang.IllegalAccessException:
-  //   class com.github.forax.pro.plugin.tester.TesterPlugin (in module com.github.forax.pro.plugin.tester)
-  // cannot access
-  //   class com.github.forax.pro.plugin.tester.TesterRunner (in module com.github.forax.pro.plugin.tester)
-  // because
-  //   module com.github.forax.pro.plugin.tester
-  // does not export
-  //   com.github.forax.pro.plugin.tester
-  // to
-  //   module com.github.forax.pro.plugin.tester
-  exports com.github.forax.pro.plugin.tester;
+  opens com.github.forax.pro.plugin.tester;
 
   // Open Test Alliance for the JVM
   requires opentest4j;
--- test.pro
+++ test.pro
@@ -1,6 +1,13 @@
 import static com.github.forax.pro.Pro.*
 
-set("pro.loglevel", "verbose");
+// run "pro" tests
+run("tester")
+
+// run "plugins" tests
+set("tester.moduleExplodedTestPath", list(path(
+  // "plugins/runner/target/test/exploded",
+  "plugins/tester/target/test/exploded"
+)))
 run("tester")
 
 /exit
