7bbeb3e5c832f149711b1e5fda0c3b06ed2f99bc
forax <forax@univ-mlv.fr>
2017-08-12 20:08:40 +0200

    suppress a "static method" warning in tester test

--- plugins/tester/src/test/java/com.github.forax.pro.plugin.tester/com/github/forax/pro/plugin/tester/TesterPluginTests.java
+++ plugins/tester/src/test/java/com.github.forax.pro.plugin.tester/com/github/forax/pro/plugin/tester/TesterPluginTests.java
@@ -3,11 +3,10 @@ package com.github.forax.pro.plugin.tester;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+@SuppressWarnings("static-method")
 class TesterPluginTests {
-
   @Test
   void nameIsTester() {
     Assertions.assertEquals("tester", new TesterPlugin().name());
   }
-
 }
