49ceaf6d91dc6521f644db8e8a3f4f2db0162994
Christian Stein <sormuras@gmail.com>
2017-11-20 18:27:08 +0100

    Upgrade to JUnit Platform Commons 1.1.0-SNAPSHOT
    The 1.1.0-SNAPSHOT version provides a better solution when it comes to
    finding test classes via "selectModule(<module name>)". See details at:
    
    https://github.com/junit-team/junit5/commit/54c90fd868787cb2884681516326e0631e6fd275
    
    Addresses #53

--- src/main/java/com.github.forax.pro.bootstrap/com/github/forax/pro/bootstrap/Bootstrap.java
+++ src/main/java/com.github.forax.pro.bootstrap/com/github/forax/pro/bootstrap/Bootstrap.java
@@ -83,7 +83,7 @@ public class Bootstrap {
       set("resolver.remoteRepositories", list(
         uri("https://oss.sonatype.org/content/repositories/snapshots")
       ));
-      String junitPlatformVersion = "1.1.0-M1";
+      String junitPlatformVersion = "1.1.0-SNAPSHOT";
       String junitJupiterVersion = "5.1.0-M1";
       String opentest4jVersion = "1.0.0";
       String apiGuardianVersion = "1.0.0";
