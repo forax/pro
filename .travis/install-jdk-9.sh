98c835d490e8cc350088cd95ebce3c1e99cfc60e
Christian Stein <sormuras@gmail.com>
2017-11-20 11:55:19 +0100

    Introduce build matrix on Travis CI using various JDKs

---
+++ .travis/install-jdk-9.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+#
+# Download and extract JDK from http://jdk.java.net/9/
+#
+
+JDK_FEATURE=9.0.1
+JDK_ARCHIVE=openjdk-${JDK_FEATURE}_linux-x64_bin.tar.gz
+
+cd ~
+wget http://download.java.net/java/GA/jdk9/${JDK_FEATURE}/binaries/${JDK_ARCHIVE}
+tar -xzf ${JDK_ARCHIVE}
+export JAVA_HOME=~/jdk-${JDK_FEATURE}
+export PATH=${JAVA_HOME}/bin:$PATH
+cd -
+java --version
