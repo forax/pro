ae1cb25d1bc422d2096a075e56e4db6c6ebabf1c
forax <forax@univ-mlv.fr>
2018-04-13 19:06:49 +0200

    varification of pro.plugin.perfer

--- plugins/perfer/src/main/java/com.github.forax.pro.plugin.perfer/com/github/forax/pro/plugin/perfer/PerferPlugin.java
+++ plugins/perfer/src/main/java/com.github.forax.pro.plugin.perfer/com/github/forax/pro/plugin/perfer/PerferPlugin.java
@@ -5,16 +5,10 @@ import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toSet;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.module.ModuleFinder;
-import java.lang.module.ModuleReader;
-import java.lang.module.ModuleReference;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
 import org.openjdk.jmh.runner.BenchmarkList;
 import org.openjdk.jmh.runner.BenchmarkListEntry;
@@ -42,37 +36,37 @@ public class PerferPlugin implements Plugin {
   
   @Override
   public void configure(MutableConfig config) {
-    PerferConf perfer = config.getOrUpdate(name(), PerferConf.class);
-    ConventionFacade convention = config.getOrThrow("convention", ConventionFacade.class); 
+    var perferConf = config.getOrUpdate(name(), PerferConf.class);
+    var convention = config.getOrThrow("convention", ConventionFacade.class); 
     
-    derive(perfer, PerferConf::moduleArtifactTestPath, convention, ConventionFacade::javaModuleArtifactTestPath);
-    derive(perfer, PerferConf::moduleDependencyPath, convention, ConventionFacade::javaModuleDependencyPath);
+    derive(perferConf, PerferConf::moduleArtifactTestPath, convention, ConventionFacade::javaModuleArtifactTestPath);
+    derive(perferConf, PerferConf::moduleDependencyPath, convention, ConventionFacade::javaModuleDependencyPath);
     
-    derive(perfer, PerferConf::javaCommand, convention,
+    derive(perferConf, PerferConf::javaCommand, convention,
         c -> c.javaHome().resolve("bin").resolve(Platform.current().javaExecutableName()));
   }
   
   @Override
   public void watch(Config config, WatcherRegistry registry) {
-    PerferConf jmher = config.getOrThrow(name(), PerferConf.class);
-    jmher.moduleArtifactTestPath().forEach(registry::watch);
-    jmher.moduleDependencyPath().forEach(registry::watch);
+    var perferConf = config.getOrThrow(name(), PerferConf.class);
+    perferConf.moduleArtifactTestPath().forEach(registry::watch);
+    perferConf.moduleDependencyPath().forEach(registry::watch);
   }
   
   @Override
   public int execute(Config config) throws IOException {
-    Log log = Log.create(name(), config.getOrThrow("pro", ProConf.class).loglevel());
-    PerferConf perfer = config.getOrThrow(name(), PerferConf.class);
+    var log = Log.create(name(), config.getOrThrow("pro", ProConf.class).loglevel());
+    var perfer = config.getOrThrow(name(), PerferConf.class);
     log.debug(config, conf -> "config " + perfer);
     
-    List<Path> modulePath = StableList.<Path>of()
+    var modulePath = StableList.<Path>of()
         .append(perfer.moduleArtifactTestPath())
         .appendAll(perfer.moduleDependencyPath());
-    Path javaCommand = perfer.javaCommand();
+    var javaCommand = perfer.javaCommand();
     
-    ModuleFinder finder = ModuleFinder.of(perfer.moduleArtifactTestPath());
-    for(ModuleReference ref :finder.findAll()) {
-      int exitCode = ref.location().map(Paths::get).map(testPath -> execute(log, testPath, modulePath, javaCommand)).orElse(0);
+    var finder = ModuleFinder.of(perfer.moduleArtifactTestPath());
+    for(var module :finder.findAll()) {
+      int exitCode = module.location().map(Paths::get).map(testPath -> execute(log, testPath, modulePath, javaCommand)).orElse(0);
       if (exitCode != 0) {
         return exitCode;
       }
@@ -81,22 +75,22 @@ public class PerferPlugin implements Plugin {
   }
   
   private static int execute(Log log, Path testPath, List<Path> modulePath, Path javaCommand) {
-    ModuleReference moduleReference = ModuleHelper.getOnlyModule(testPath);
-    String moduleName = moduleReference.descriptor().name();
+    var moduleReference = ModuleHelper.getOnlyModule(testPath);
+    var moduleName = moduleReference.descriptor().name();
     log.debug(moduleName, name -> "found test module " + name);
     
-    try(ModuleReader reader = moduleReference.open()) {
-      Optional<InputStream> input = reader.open("META-INF/BenchmarkList");
-      if (!input.isPresent()) {
+    try(var reader = moduleReference.open()) {
+      var inputOpt = reader.open("META-INF/BenchmarkList");
+      if (!inputOpt.isPresent()) {
         return 0;
       }
       
-      Collection<BenchmarkListEntry> list = BenchmarkList.readBenchmarkList(input.get());
-      Set<String> classNames = list.stream().map(BenchmarkListEntry::getUserClassQName).collect(toSet());
+      var list = BenchmarkList.readBenchmarkList(inputOpt.get());
+      var classNames = list.stream().map(BenchmarkListEntry::getUserClassQName).collect(toSet());
       log.debug(classNames, names -> "benchmarks " + classNames);
       
-      for(String className: classNames) {
-        int exitCode = executeClass(moduleName, className, modulePath, javaCommand);
+      for(var className: classNames) {
+        var exitCode = executeClass(moduleName, className, modulePath, javaCommand);
         if (exitCode != 0) {
           return exitCode;
         }
@@ -110,7 +104,7 @@ public class PerferPlugin implements Plugin {
   }
   
   private static int executeClass(String moduleName, String className, List<Path> modulePath, Path javaCommand) throws IOException {
-    Process process = new ProcessBuilder(
+    var process = new ProcessBuilder(
         StableList.of(javaCommand.toString()).appendAll(StableList.of("--module-path",
             modulePath.stream().map(Path::toString).collect(joining(":")), "-m", moduleName + '/' + className)))
                 .redirectErrorStream(true).start();
