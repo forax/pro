71fb8f647c3b2274c1c3bf9d5ebfdcc7db9417a1
forax <forax@univ-mlv.fr>
2018-04-13 16:20:15 +0200

    link to java 10 javadoc

--- src/main/java/com.github.forax.pro.bootstrap/com/github/forax/pro/bootstrap/Bootstrap.java
+++ src/main/java/com.github.forax.pro.bootstrap/com/github/forax/pro/bootstrap/Bootstrap.java
@@ -85,7 +85,7 @@ public class Bootstrap {
         ));
 
     set("docer.quiet", true);
-    set("docer.link", uri("https://docs.oracle.com/javase/9/docs/api/"));
+    set("docer.link", uri("https://docs.oracle.com/javase/10/docs/api/"));
 
     set("linker.includeSystemJMODs", true);
     set("linker.launchers", list(
