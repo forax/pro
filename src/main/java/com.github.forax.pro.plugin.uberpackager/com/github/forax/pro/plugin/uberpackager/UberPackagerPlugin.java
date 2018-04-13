57f87e0341b4f68ccdde76fa39ababbb14e3cd2e
forax <forax@univ-mlv.fr>
2018-04-13 18:41:33 +0200

    varification of pro.plugin.uberpackager

--- src/main/java/com.github.forax.pro.plugin.uberpackager/com/github/forax/pro/plugin/uberpackager/UberPackagerPlugin.java
+++ src/main/java/com.github.forax.pro.plugin.uberpackager/com/github/forax/pro/plugin/uberpackager/UberPackagerPlugin.java
@@ -2,15 +2,11 @@ package com.github.forax.pro.plugin.uberpackager;
 
 import static com.github.forax.pro.api.MutableConfig.derive;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.lang.module.ModuleReader;
-import java.lang.module.ModuleReference;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.spi.ToolProvider;
-import java.util.stream.Stream;
 
 import com.github.forax.pro.api.Config;
 import com.github.forax.pro.api.MutableConfig;
@@ -21,7 +17,6 @@ import com.github.forax.pro.api.helper.ProConf;
 import com.github.forax.pro.helper.FileHelper;
 import com.github.forax.pro.helper.Log;
 import com.github.forax.pro.helper.util.StableList;
-import com.github.forax.pro.ubermain.Main;
 
 public class UberPackagerPlugin implements Plugin {
   @Override
@@ -36,47 +31,46 @@ public class UberPackagerPlugin implements Plugin {
   
   @Override
   public void configure(MutableConfig config) {
-    UberPackagerConf packager = config.getOrUpdate(name(), UberPackagerConf.class);
-    ConventionFacade convention = config.getOrThrow("convention", ConventionFacade.class); 
+    var uberPackagerConf = config.getOrUpdate(name(), UberPackagerConf.class);
+    var convention = config.getOrThrow("convention", ConventionFacade.class); 
     
     // inputs
-    derive(packager, UberPackagerConf::moduleArtifactSourcePath, convention, ConventionFacade::javaModuleArtifactSourcePath);
-    derive(packager, UberPackagerConf::moduleDependencyPath, convention, ConventionFacade::javaModuleDependencyPath);
+    derive(uberPackagerConf, UberPackagerConf::moduleArtifactSourcePath, convention, ConventionFacade::javaModuleArtifactSourcePath);
+    derive(uberPackagerConf, UberPackagerConf::moduleDependencyPath, convention, ConventionFacade::javaModuleDependencyPath);
     
     // outputs
-    derive(packager, UberPackagerConf::moduleUberPath, convention, ConventionFacade::javaModuleUberPath);
-    derive(packager, UberPackagerConf::moduleUberExplodedPath, convention, ConventionFacade::javaModuleUberExplodedPath);
+    derive(uberPackagerConf, UberPackagerConf::moduleUberPath, convention, ConventionFacade::javaModuleUberPath);
+    derive(uberPackagerConf, UberPackagerConf::moduleUberExplodedPath, convention, ConventionFacade::javaModuleUberExplodedPath);
   }
   
   @Override
   public void watch(Config config, WatcherRegistry registry) {
-    UberPackagerConf packager = config.getOrThrow(name(), UberPackagerConf.class);
-    packager.moduleArtifactSourcePath().forEach(registry::watch);
-    packager.moduleDependencyPath().forEach(registry::watch);
+    var uberPackagerConf = config.getOrThrow(name(), UberPackagerConf.class);
+    uberPackagerConf.moduleArtifactSourcePath().forEach(registry::watch);
+    uberPackagerConf.moduleDependencyPath().forEach(registry::watch);
   }
   
   @Override
   public int execute(Config config) throws IOException {
-    Log log = Log.create(name(), config.getOrThrow("pro", ProConf.class).loglevel());
+    var log = Log.create(name(), config.getOrThrow("pro", ProConf.class).loglevel());
     log.debug(config, conf -> "config " + config);
     
     log.info(null, __ -> "WARNING: this feature is highly experimental !");
     
-    ToolProvider jarTool = ToolProvider.findFirst("jar")
-        .orElseThrow(() -> new IllegalStateException("can not find the command jar"));
-    UberPackagerConf packager = config.getOrThrow(name(), UberPackagerConf.class);
+    var jarTool = ToolProvider.findFirst("jar").orElseThrow(() -> new IllegalStateException("can not find the command jar"));
+    var uberPackagerConf = config.getOrThrow(name(), UberPackagerConf.class);
     
-    Path uberExplodedPath = packager.moduleUberExplodedPath();
+    var uberExplodedPath = uberPackagerConf.moduleUberExplodedPath();
     FileHelper.deleteAllFiles(uberExplodedPath, false);
     Files.createDirectories(uberExplodedPath);
     
-    Class<Main> mainClass = com.github.forax.pro.ubermain.Main.class;
-    Module uberjarModule = mainClass.getModule();
-    ModuleReference uberJarRef = uberjarModule.getLayer().configuration().findModule(uberjarModule.getName()).get().reference();
-    try(ModuleReader moduleReader = uberJarRef.open()) {
-      String mainClassName = mainClass.getName().replace('.', '/');
-      for(String filename: List.of(mainClassName + ".class", mainClassName + "$1.class", mainClassName + "$1$1.class")) {
-        Path path = uberExplodedPath.resolve(filename);
+    var mainClass = com.github.forax.pro.ubermain.Main.class;
+    var uberjarModule = mainClass.getModule();
+    var uberJarRef = uberjarModule.getLayer().configuration().findModule(uberjarModule.getName()).get().reference();
+    try(var moduleReader = uberJarRef.open()) {
+      var mainClassName = mainClass.getName().replace('.', '/');
+      for(var filename: List.of(mainClassName + ".class", mainClassName + "$1.class", mainClassName + "$1$1.class")) {
+        var path = uberExplodedPath.resolve(filename);
         Files.createDirectories(path.getParent());
         Files.copy(moduleReader.open(filename).get(), path);
       }
@@ -84,17 +78,17 @@ public class UberPackagerPlugin implements Plugin {
     
     //FIXME add the class of module uberbooter
     
-    List<Path> modulePaths = StableList.of(packager.moduleArtifactSourcePath())
-        .appendAll(packager.moduleDependencyPath());
+    var modulePaths = StableList.of(uberPackagerConf.moduleArtifactSourcePath())
+        .appendAll(uberPackagerConf.moduleDependencyPath());
     
-    try(BufferedWriter writer = Files.newBufferedWriter(uberExplodedPath.resolve("modules.txt"))) {
+    try(var writer = Files.newBufferedWriter(uberExplodedPath.resolve("modules.txt"))) {
       writer.write("com.github.forax.pro.main/com.github.forax.pro.main.Main");
       writer.newLine();
       //writer.write(uberbooterModule);
       //writer.newLine();
-      for(Path modulePath: modulePaths) {
-        try(Stream<Path> modularJars = Files.list(modulePath)) {
-          for(Path modularJar: (Iterable<Path>)modularJars::iterator){
+      for(var modulePath: modulePaths) {
+        try(var modularJars = Files.list(modulePath)) {
+          for(var modularJar: (Iterable<Path>)modularJars::iterator){
             //System.out.println("modularJar " + modularJar);
             writer.write(modularJar.getFileName().toString());
             writer.newLine();
@@ -103,19 +97,19 @@ public class UberPackagerPlugin implements Plugin {
       }
     }
     
-    Path uberjar = packager.moduleUberPath().resolve("uber.jar");
-    CmdLine cmdLine = new CmdLine().addAll(
+    var uberjar = uberPackagerConf.moduleUberPath().resolve("uber.jar");
+    var cmdLine = new CmdLine().addAll(
         "--create",
         "--file", uberjar.toString(),
         "--main-class", mainClass.getName(),
         "-C", uberExplodedPath.toString(),
         "."
         );
-    String[] arguments = cmdLine.toArguments();
+    var arguments = cmdLine.toArguments();
     log.verbose(arguments, args -> "jar " + String.join(" ", args));
     jarTool.run(System.out, System.err, arguments);
     
-    for(Path modulePath: modulePaths) {
+    for(var modulePath: modulePaths) {
       cmdLine = new CmdLine().addAll(
           "--update",
           "--file", uberjar.toString(),
