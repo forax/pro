1f147aeace4583da5e9da936ce234f3c4c4a054f
forax <forax@univ-mlv.fr>
2018-04-13 19:08:51 +0200

    varification of pro.plugin.formatter

--- plugins/formatter/src/main/java/com.github.forax.pro.plugin.formatter/com/github/forax/pro/plugin/formatter/FormatterPlugin.java
+++ plugins/formatter/src/main/java/com.github.forax.pro.plugin.formatter/com/github/forax/pro/plugin/formatter/FormatterPlugin.java
@@ -4,6 +4,12 @@ import static com.github.forax.pro.api.MutableConfig.derive;
 import static com.github.forax.pro.helper.FileHelper.pathFilenameEndsWith;
 import static com.github.forax.pro.helper.FileHelper.walkIfNecessary;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import com.github.forax.pro.api.Config;
 import com.github.forax.pro.api.MutableConfig;
 import com.github.forax.pro.api.Plugin;
@@ -14,12 +20,6 @@ import com.github.forax.pro.helper.Log;
 import com.github.forax.pro.helper.Platform;
 import com.github.forax.pro.helper.util.StableList;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 public class FormatterPlugin implements Plugin {
   @Override
   public String name() {
@@ -28,23 +28,23 @@ public class FormatterPlugin implements Plugin {
 
   @Override
   public void init(MutableConfig config) {
-    FormatterConf formatter = config.getOrUpdate(name(), FormatterConf.class);
-    formatter.replace(false);
-    formatter.dryRun(false);
-    formatter.setExitIfChanged(false);
+    var formatterConf = config.getOrUpdate(name(), FormatterConf.class);
+    formatterConf.replace(false);
+    formatterConf.dryRun(false);
+    formatterConf.setExitIfChanged(false);
   }
   
   @Override
   public void configure(MutableConfig config) {
-    FormatterConf formatter = config.getOrUpdate(name(), FormatterConf.class);
-    ConventionFacade convention = config.getOrThrow("convention", ConventionFacade.class);
+    var formatterConf = config.getOrUpdate(name(), FormatterConf.class);
+    var convention = config.getOrThrow("convention", ConventionFacade.class);
 
-    derive(formatter, FormatterConf::moduleSourcePath, convention, ConventionFacade::javaModuleSourcePath);
-    derive(formatter, FormatterConf::moduleTestPath, convention, ConventionFacade::javaModuleTestPath);
+    derive(formatterConf, FormatterConf::moduleSourcePath, convention, ConventionFacade::javaModuleSourcePath);
+    derive(formatterConf, FormatterConf::moduleTestPath, convention, ConventionFacade::javaModuleTestPath);
   }
 
   private static List<Path> listAllJavaFiles(FormatterConf formatter) {
-    StableList<Path> sourcePaths = StableList.<Path>of()
+    var sourcePaths = StableList.<Path>of()
         .appendAll(formatter.moduleSourcePath())
         .appendAll(formatter.moduleTestPath());
     return walkIfNecessary(sourcePaths, pathFilenameEndsWith(".java"));
@@ -52,38 +52,38 @@ public class FormatterPlugin implements Plugin {
   
   @Override
   public void watch(Config config, WatcherRegistry registry) {
-    FormatterConf formatter = config.getOrThrow(name(), FormatterConf.class);
-    formatter.moduleSourcePath().forEach(registry::watch);
-    formatter.moduleTestPath().forEach(registry::watch);
+    var formatterConf = config.getOrThrow(name(), FormatterConf.class);
+    formatterConf.moduleSourcePath().forEach(registry::watch);
+    formatterConf.moduleTestPath().forEach(registry::watch);
   }
 
   @Override
   public int execute(Config config) throws IOException {
-    Log log = Log.create(name(), config.getOrThrow("pro", ProConf.class).loglevel());
-    ConventionFacade convention = config.getOrThrow("convention", ConventionFacade.class);
-    FormatterConf formatter = config.getOrThrow(name(), FormatterConf.class);
-    log.debug(formatter, _formatter -> "config " + _formatter);
+    var log = Log.create(name(), config.getOrThrow("pro", ProConf.class).loglevel());
+    var convention = config.getOrThrow("convention", ConventionFacade.class);
+    var formatterConf = config.getOrThrow(name(), FormatterConf.class);
+    log.debug(formatterConf, _formatterConf -> "config " + _formatterConf);
 
-    CmdLine cmdLine = new CmdLine();
+    var cmdLine = new CmdLine();
     cmdLine.add(convention.javaHome().resolve("bin").resolve(Platform.current().javaExecutableName()));
     cmdLine.add("--module-path");
     cmdLine.add(convention.javaHome().resolve("plugins").resolve(name()).resolve("libs"));
     cmdLine.add("--module").add("google.java.format");
     
-    Optional.of("--replace").filter(__ -> formatter.replace()).ifPresent(cmdLine::add);
-    Optional.of("--dry-run").filter(__ -> formatter.dryRun()).ifPresent(cmdLine::add);
-    Optional.of("--set-exit-if-changed").filter(__ -> formatter.setExitIfChanged()).ifPresent(cmdLine::add);
+    Optional.of("--replace").filter(__ -> formatterConf.replace()).ifPresent(cmdLine::add);
+    Optional.of("--dry-run").filter(__ -> formatterConf.dryRun()).ifPresent(cmdLine::add);
+    Optional.of("--set-exit-if-changed").filter(__ -> formatterConf.setExitIfChanged()).ifPresent(cmdLine::add);
     
     // no arguments will trigger default "format files to stdout" mode
-    formatter.rawArguments().ifPresent(args -> args.forEach(cmdLine::add));
+    formatterConf.rawArguments().ifPresent(args -> args.forEach(cmdLine::add));
     log.verbose(cmdLine, CmdLine::toString);
     
     // files
-    List<Path> files = formatter.files().orElseGet(() -> listAllJavaFiles(formatter));
+    var files = formatterConf.files().orElseGet(() -> listAllJavaFiles(formatterConf));
     log.debug(files, fs -> "files:\n" + fs.stream().map(Path::toString).collect(Collectors.joining(" ")));
     files.forEach(cmdLine::add);
 
-    Process process = new ProcessBuilder(cmdLine.toArguments()).redirectErrorStream(true).start();
+    var process = new ProcessBuilder(cmdLine.toArguments()).redirectErrorStream(true).start();
     process.getInputStream().transferTo(System.out);
     try {
       return process.waitFor();
