e053167aec7e4d263ad2141c99335217b110f64e
forax <forax@univ-mlv.fr>
2018-05-27 10:58:27 +0200

    travis: fix JAVA_HOME for macos

--- install-macos.sh
+++ install-macos.sh
@@ -1,6 +1,6 @@
 wget https://download.java.net/java/GA/jdk10/10.0.1/fb4372174a714e6b8c52526dc134031e/10/openjdk-10.0.1_osx-x64_bin.tar.gz
 tar -xf openjdk-10.0.1_osx-x64_bin.tar.gz
 
-export JAVA_HOME=openjdk-10.0.1_osx-x64
+export JAVA_HOME=jdk-10.0.1.jdk/Contents/Home/
 $JAVA_HOME/bin/java --version
 
