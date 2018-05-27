24e286ce1538070fc8376670c893ab9202242a9f
forax <forax@univ-mlv.fr>
2018-05-27 10:47:57 +0200

    travis: fix macos install

---
+++ install-macos.sh
@@ -0,0 +1,6 @@
+wget https://download.java.net/java/GA/jdk10/10.0.1/fb4372174a714e6b8c52526dc134031e/10/openjdk-10.0.1_osx-x64_bin.tar.gz
+tar -xf openjdk-10.0.1_osx-x64_bin.tar.gz
+
+export JAVA_HOME=openjdk-10.0.1_osx-x64
+$JAVA_HOME/bin/java --version
+
