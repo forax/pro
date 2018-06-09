fcf0ec72d727fbf8810466ce917a74838bc9368e
forax <forax@univ-mlv.fr>
2018-06-10 00:30:14 +0200

    add Lazy and Strategy to api util

---
+++ src/main/java/com.github.forax.pro.helper/com/github/forax/pro/helper/util/Lazy.java
@@ -0,0 +1,37 @@
+package com.github.forax.pro.helper.util;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+/**
+ * Represent a lazy evaluation of a value.
+ *
+ * @param <V> the type of the value
+ */
+public interface Lazy<V> {
+  /**
+   * Evaluate the value if the value was not evaluated yet.
+   * @return the value.
+   */
+  public V eval();
+
+  /**
+   * Create a lazy evaluation of a value from a computation (a supplier).
+   * @param supplier the computation of the value, should be side effect free.
+   * @return the result of the computation
+   */
+  static <V> Lazy<V> lazy(Supplier<? extends V> supplier) {
+    Objects.requireNonNull(supplier);
+    return new Lazy<>() {
+      private V value;
+
+      @Override
+      public V eval() {
+        if (value != null) {
+          return value;
+        }
+        return value = supplier.get();
+      }
+    };
+  }
+}
---
+++ src/main/java/com.github.forax.pro.helper/com/github/forax/pro/helper/util/Strategy.java
@@ -0,0 +1,38 @@
+package com.github.forax.pro.helper.util;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ *  Represent a strategy of evaluation.
+ *
+ * @param <V> the type of parameter of the evaluation.
+ * @param <R> the type of the result of the evaluation
+ */
+public interface Strategy<V, R> {
+  /**
+   * Try to evaluate the value, it can fails in that case {@link Optional#empty()} is returned.
+   * @param value the parameter to evaluate.
+   * @return the result of the evaluation or empty.
+   */
+  Optional<R> get(V value);
+  
+  /**
+   * Create a strategy that try each strategy in the order of the array.
+   * @param strategies an array of strategies
+   * @return a strategy that will delegate its computation to several strategies.
+   */
+  @SafeVarargs
+  static <V, R> Strategy<V, R> compose(Strategy<V, R>... strategies) {
+    List<Strategy<V, R>> list = List.of(strategies);
+    return value -> {
+      for(Strategy<V, R> strategy: list) {
+        var result = strategy.get(value);
+        if (result.isPresent()) {
+          return result;
+        }
+      }
+      return Optional.empty();
+    };
+  }
+}
