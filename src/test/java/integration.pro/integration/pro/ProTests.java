99ae0ca41fb2ff53c93827d4fea233e217d4faa4
Christian Stein <sormuras@gmail.com>
2018-06-25 22:36:08 +0200

    Update reason with more descriptive message

--- src/test/java/integration.pro/integration/pro/ProTests.java
+++ src/test/java/integration.pro/integration/pro/ProTests.java
@@ -9,7 +9,12 @@ import org.junit.jupiter.api.Test;
 @SuppressWarnings("static-method")
 class ProTests {
   @Test
-  @Disabled("java.util.ServiceConfigurationError: com.github.forax.pro.api.Plugin: com.github.forax.pro.plugin.modulefixer.ModuleFixerPlugin not a subtype")
+  @Disabled(
+      "java.lang.LinkageError: loader constraint violation: loader \"<unnamed>\""
+          + "(instance of jdk.internal.loader.Loader, child of \"app\" jdk.internal.loader.ClassLoaders$AppClassLoader)"
+          + "wants to load class com.github.forax.pro.helper.util.StableList."
+          + "A different class with the same name was previously loaded by \"app\""
+          + "(instance of jdk.internal.loader.ClassLoaders$AppClassLoader)")
   void list() {
     assertEquals(0, Pro.list().size());
   }
