f51f0af975664faee2c8194774c11b8de8772bfc
forax <forax@univ-mlv.fr>
2018-10-02 15:43:23 +0200

    scaffold: fix docer default URL

--- src/main/java/com.github.forax.pro.main/com/github/forax/pro/main/Scaffold.java
+++ src/main/java/com.github.forax.pro.main/com/github/forax/pro/main/Scaffold.java
@@ -53,7 +53,7 @@ class Scaffold {
         "\n" + 
         "docer.\n" + 
         "    quiet(true).\n" + 
-        "    link(uri(\"https://docs.oracle.com/en/java/javase/11/docs/api/index.html\"))\n" + 
+        "    link(uri(\"https://docs.oracle.com/en/java/javase/11/docs/api/\"))\n" + 
         "   \n" + 
         "packager.\n" + 
         "    modules(\n" + 
