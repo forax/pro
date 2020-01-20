6f6f4dc7b0c1c4de13cedbfb719196a86d9fa490
Christian Stein <sormuras@gmail.com>
2020-01-20 22:23:54 +0100

    Upgrade to JUnit 5.6

--- src/main/java/com.github.forax.pro.bootstrap/com/github/forax/pro/bootstrap/Bootstrap.java
+++ src/main/java/com.github.forax.pro.bootstrap/com/github/forax/pro/bootstrap/Bootstrap.java
@@ -107,8 +107,8 @@ public class Bootstrap {
     compileAndPackagePlugin("runner", list("resolver", "modulefixer", "compiler", "packager"), () -> { /* empty */});
     compileAndPackagePlugin("tester", list("resolver", "modulefixer", "compiler", "packager"), () -> {
       // set("resolver.remoteRepositories", list(uri("https://oss.sonatype.org/content/repositories/snapshots")));
-      var junitPlatformVersion = "1.5.0-RC2";
-      var junitJupiterVersion = "5.5.0-RC2";
+      var junitPlatformVersion = "1.6.0";
+      var junitJupiterVersion = "5.6.0";
       var opentest4jVersion = "1.2.0";
       var apiGuardianVersion = "1.1.0";
       set("resolver.dependencies", list(
