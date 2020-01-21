8eaa1a69b4a736d0c75a2e6b1877ce760d580136
forax <forax@univ-mlv.fr>
2020-01-21 08:49:28 +0100

    update JMH version of the perfer plugin

--- src/main/java/com.github.forax.pro.bootstrap/com/github/forax/pro/bootstrap/Bootstrap.java
+++ src/main/java/com.github.forax.pro.bootstrap/com/github/forax/pro/bootstrap/Bootstrap.java
@@ -125,7 +125,7 @@ public class Bootstrap {
       ));
     });
     compileAndPackagePlugin("perfer", list("resolver", "modulefixer", "compiler", "packager"), () -> {
-      var jmhVersion = "1.21";
+      var jmhVersion = "1.22";
       var commonMath3Version = "3.6.1";
       var joptSimpleVersion = "5.0.4";
       set("resolver.dependencies", list(
@@ -177,7 +177,7 @@ public class Bootstrap {
     //com.github.forax.pro.bootstrap.genbuilder.GenBuilder.generate();
     
     // update module name to maven coordinates list
-    //updateModuleNameList();
+    // updateModuleNameList();
     
     Vanity.postOperations();
   }
